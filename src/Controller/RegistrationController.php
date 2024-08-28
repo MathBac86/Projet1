692f0485b666dcdf8324b7d873e47d647286ed42
MatBac <matbaconnet@gmail.com>
2024-08-28 13:20:25 +0200

    user

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/login', name: 'app_registration')]
+    public function index(UserPasswordHasherInterface $passwordHasher): Response
+    {
+
+        // ... e.g. get the user data from a registration form
+        // $user = new User(...);
+        // $plaintextPassword = ...;
+
+        // // hash the password (based on the security.yaml config for the $user class)
+        // $hashedPassword = $passwordHasher->hashPassword(
+        //     $user,
+        //     $plaintextPassword
+        // );
+        // $user->setPassword($hashedPassword);
+
+
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+}
